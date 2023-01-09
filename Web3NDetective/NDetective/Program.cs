68cc715bc63c5f67d86a20eedc9bfbea57d3a6bc
dr.lis <dr.lis@outlook.com>
2023-01-09 03:32:27 +0300

    Added base parsing

--- Web3NDetective/NDetective/Program.cs
+++ Web3NDetective/NDetective/Program.cs
@@ -6,7 +6,16 @@ namespace NDetective
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var indexOfExplorerParam = Array.IndexOf(args, "--explorer") + 1;
+			String explorerUrl = args[indexOfExplorerParam];
+			var indexOfAddresses = Array.IndexOf(args, "--contract-addresses") + 1;
+			String addressesOdContracts = args[indexOfAddresses];
+			var addresses = addressesOdContracts.Split(',');
+			var indexOfApiKey = Array.IndexOf(args, "--api-key") + 1;
+			String apiKey = args[indexOfApiKey];
+
+			TxParser parser = new TxParser(explorerUrl, apiKey, addresses);
+			parser.Parse();
 		}
 	}
 }
---
+++ Web3NDetective/NDetective/TxParser.cs
@@ -0,0 +1,40 @@
+﻿using EthScanNet.Lib;
+using EthScanNet.Lib.Models.EScan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDetective
+{
+	class TxParser
+	{
+		public EScanClient client;
+		public HashSet<String> addressesForParsing;
+
+		public TxParser(string explorerUrl, string apiKey, string[] addressesOfContracts)
+		{
+			EScanNetwork eScanNetwork = new EScanNetwork(explorerUrl, false);
+			client = new EScanClient(eScanNetwork, apiKey, null);
+			addressesForParsing = new HashSet<String>();
+			foreach (String address in addressesOfContracts)
+			{
+				addressesForParsing.Add(address);
+			}
+		}
+
+		public void Parse()
+		{
+			foreach (var contractAddress in addressesForParsing)
+			{
+				EScanAddress eScanAddress = new EScanAddress(contractAddress);
+				var result = client.Accounts.GetNormalTransactionsAsync(eScanAddress).Result;
+				foreach (var tx in result.Transactions)
+				{
+					Console.WriteLine($"{tx.From}\t{tx.ContractAddress}\t{tx.BlockNumber}\t{tx.To}\t{tx.Input}");
+				}
+			}
+		}
+	}
+}
